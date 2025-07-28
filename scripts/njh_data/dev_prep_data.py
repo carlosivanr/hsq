3f9f2c9f26ab701978c78231a3f55cea62597e6a
rodrica2 <carlos.i.rodriguez@cuanschutz.edu>
2025-07-28 15:11:19 -0600

    Modified the njh data eda script to consider that some states didn't ask all questions.

--- scripts/njh_data/dev_prep_data.py
+++ scripts/njh_data/dev_prep_data.py
@@ -4,7 +4,7 @@
 # HSQ - import and prep NJH data
 #
 # Description:
-# The purpose of this script is import the NJH data extracts. The NJH data
+# The purpose of this script is import the NJH data extract. The NJH data
 # extract is a file that combines the state specific quitline intake questions.
 # 
 # A secondary purpose of this script is to rename the NJH extract columns.
@@ -18,6 +18,7 @@
  
 # Import libraries ------------------------------------------------------------
 import pandas as pd
+# import numpy as np
 import os
 
 # Read in the data extract ---------------------------------------------------
@@ -25,50 +26,60 @@ proj_root = 'C:\\Users\\rodrica2\\OneDrive - The University of Colorado Denver\\
 
 sub_dir = '\\scripts\\njh_data\\codebooks'
 
-# data_dir = '\\scripts\\njh_data\\Helpers Quit Study Data Extract 10.10.24.xlsx'
-data_dir = '\\scripts\\njh_data\\Helpers Quit Study Data Extract 11.13.24.xlsx'
+data_dir = '\\scripts\\njh_data\\Helpers Quit Study Data Extract 01.06.25.xlsx'
 
 file_path = proj_root + data_dir
 
 data = pd.read_excel(file_path, header = 1)
 
 
-# Get column names ------------------------------------------------------------
-# Get the names from the data extract of the columns that need question labels
-# Columns that need to be determined are AI1 through UT 12
-
-# This is the column range for an older version of the data extract
-# 10-10-2024
-# col_names = pd.DataFrame({'MDS Question Id': data.loc[:, 'AI 1':'UT 12'].columns,
-#                           'Question Text': pd.NA})
-
-# 11-13-2024
-col_names = pd.DataFrame({'MDS Question Id': data.loc[:, 'NJ 31':'UT 9'].columns,
+# Get question ids ------------------------------------------------------------
+# Capture the question ids of NJ 31 through UT 9 and turn them into a dataframe
+# where the the text is set to NA. These are the question ids for which the 
+# question text needs to be determined. The question text will be found in the 
+# codebooks, where there are two codebooks (intake and eligibility) for every 
+# state.
+question_id_map = pd.DataFrame({'MDS Question Id': data.loc[:, 'NJ 31':'UT 9'].columns,
                           'Question Text': pd.NA})
 
 
 # Import the code book for the data extract -----------------------------------
 # Each state has a different code book, therefore list all of the .xlsx files
-# to loop through each file and merge in the required column names
+# to loop through each file and merge in the required question text for each
+# question id
 files = pd.DataFrame({'file': os.listdir(proj_root + sub_dir)})
 
-# Create the state column
+# Create the state column by slicing the string for the first two letters
 files['state'] = files['file'].str.slice(0,2)
 
-# for each file in files
-# read in the .xlsx file
-# skip the first 9 rows
-# remove columns that start with '...'
-# Only fill in the values of the column
-# names that remain to be answered
 
-# Outputs to a data frame named col_names
+# Create an empty data frame that will represent a map of which questions were
+# asked by a given state.
+state_question_map = pd.DataFrame()
+
 for x in files['file']:
+  # This for-loop is designed to read in each state's .xlsx codebook. The first
+  # 9 rows are skipped because they are formatted header rows, and do not 
+  # contain data of interest. For each codebook, the questions in question_id_map
+  # are cross referenced and only the questions in question_id_map that need a label
+  # are scanned, pulled out and then the question text is merged into question_id_map
   print(x)
+  # Set the path root
   path = proj_root + sub_dir + '\\'
+
+  # Read in the file using path as temp
   temp = pd.read_excel(path + x, header = 9)
 
-  to_be_filled = col_names[col_names['Question Text'].isna()][['MDS Question Id']]
+  # Capture the MDS Question Ids, add the state, then append to the question 
+  # map
+  questions = pd.DataFrame(temp['MDS Question Id'].dropna())
+  state = files[files['file'] == x]['state'].iloc[0]
+  questions['state'] = state
+  state_question_map = pd.concat([state_question_map, questions], ignore_index = True)
+
+
+  # Get a list of column names that need to have the question text
+  to_be_filled = question_id_map[question_id_map['Question Text'].isna()][['MDS Question Id']]
 
   if to_be_filled.shape[0] > 0:
     # Filter the temp data frame to only the rows that need to be assigned a Question Text
@@ -88,34 +99,87 @@ for x in files['file']:
 
     output = output.dropna()
 
-    col_names = pd.concat([output, 
-                          col_names[~col_names['MDS Question Id']
+    question_id_map = pd.concat([output, 
+                          question_id_map[~question_id_map['MDS Question Id']
                                     .isin(output['MDS Question Id'])]], 
                           ignore_index=True)
 
 
+# question_id_map represents a cross walk from the question id such as NJ31 to 
+# its corresponding question text. Although each state asks a slightly 
+# different set of question ids, all question ids of interest are found in the
+# question_id_map data frame which are derived from the data extract. So the 
+# state question map should be filtered to the rows where MDS Quesiont Id is 
+# in question_id_map
+state_question_map = (state_question_map[state_question_map["MDS Question Id"]
+  .isin(question_id_map["MDS Question Id"])])
+
+# Why do some counts have over 100
+(state_question_map
+  .groupby("state")
+  .size())
+
+az_questions = state_question_map[state_question_map['state'] == "AZ"]
+
+# * Some are in duplicates and some are in triplicates
+# Some states, AZ UT 9 for example, are asked 3x according to the intake 
+# codebook.
+# Row 435 - Default
+# Row 788 - American Indian
+# Row 1226 - Youth
+
+# Check that each state does in fact have a different number of questions
+state_counts = (state_question_map
+  .drop_duplicates()
+  .groupby("state")['MDS Question Id']
+  .count())
+
+# * MA asks two additional questions out of the current list of states
+# as of 07/28/2025
+
+state_question_map['asked'] = 1  # Create a column of 1s
+
+state_question_map = state_question_map.drop_duplicates()
+
+# Pivot wider with one row per MDS Question Id, and a column per state
+result = state_question_map.pivot_table(index='MDS Question Id', 
+                        columns='state', 
+                        values='asked', 
+                        aggfunc='max', 
+                        fill_value=0)
+
+# The questions that MA asks that are additional ar OI 7b and OI 7f, which asks
+# if cigars or vaping in the parent question are  menthol
+
+# Save the state_question_map to .csv
+output_dir = '\\scripts\\njh_data\\'
+
+# Output the unlabeled data
+data_out = proj_root + output_dir + 'state_question_map.csv'
+state_question_map.to_csv(data_out, index=False)
+
+state_question_map.groupby('MDS Question Id')['state'].nunique().reset_index(name='n_unique')
+
 # Reformat the primary data frame ---------------------------------------------
 # Replace the acronym names with human radable labels
 
-# Sort col_names to match the order in which they appear in the primary df
-col_names = col_names.sort_values(by = 'MDS Question Id')
+# Sort question_id_map to match the order in which they appear in the primary df
+question_id_map = question_id_map.sort_values(by = 'MDS Question Id')
 
 # Modify the question text for those with a lot of text and/or contains html
-col_names.loc[col_names["MDS Question Id"] == 'SI 1', 'Question Text'] = "How may I help you today"
-col_names.loc[col_names["MDS Question Id"] == 'SI 18e', 'Question Text'] = "Are you American Indian or Alaska Native?"
-col_names.loc[col_names["MDS Question Id"] == 'OI 24', 'Question Text'] = "Do you consider yourself to be gay, lesbian or bisexual?"
+question_id_map.loc[question_id_map["MDS Question Id"] == 'SI 1', 'Question Text'] = "How may I help you today"
+question_id_map.loc[question_id_map["MDS Question Id"] == 'SI 18e', 'Question Text'] = "Are you American Indian or Alaska Native?"
+question_id_map.loc[question_id_map["MDS Question Id"] == 'OI 24', 'Question Text'] = "Do you consider yourself to be gay, lesbian or bisexual?"
 
 
 # Create a dictionary for renaming columns
-mapper = dict(zip(col_names["MDS Question Id"], col_names["Question Text"]))
+mapper = dict(zip(question_id_map["MDS Question Id"], question_id_map["Question Text"]))
 
 # Create a new data frame with labeled columns by applying the mapper to the
 # column names
 data_labeled = data.rename(columns = mapper)
 
 # Output data to a .csv -------------------------------------------------------
-output_dir = '\\scripts\\njh_data\\'
-
 # Output the unlabeled data
 data_out = proj_root + output_dir + 'njh_data.csv'
 data.to_csv(data_out, index=False)
@@ -126,5 +190,5 @@ mapper.index.name = 'question'
 mapper = mapper.rename(columns = {0: 'label'})
 mapper = mapper.reset_index()
 
-mapper_out = proj_root + output_dir + 'mapper.csv'
+mapper_out = proj_root + output_dir + 'text_question_map.csv'
 mapper.to_csv(mapper_out)
